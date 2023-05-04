8ed217ff3487bbdd7237146176de9ca48f7b7d4b
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-05-04 15:00:32 +0200

    Add download archive function

--- classy/tools.py
+++ classy/tools.py
@@ -1,4 +1,30 @@
+import tarfile
+from urllib.request import urlretrieve
+import urllib
+from zipfile import ZipFile
+
 import numpy as np
+
+from classy import progress
+from classy.log import logger
+
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    Progress,
+    TextColumn,
+    # MofNCompleteColumn,
+)
+
+
+def _retrieve_from_github(host, which, path):
+    URL = f"https://raw.githubusercontent.com/maxmahlke/classy/main/data/{host}/{which}.csv"
+    path.parent.mkdir(exist_ok=True)
+
+    try:
+        urlretrieve(URL, path)
+    except:
+        urlretrieve(URL.replace("main", "develop"), path)
 
 
 def find_nearest(array, value):
@@ -20,20 +46,60 @@ def download_archive(URL, PATH_ARCHIVE, unpack=True, remove=True):
 
     Parameters
     ----------
-    columns : list
-        List of str with column names.
-
-    Returns
-    -------
-    list
-        List containing all string elements which are numeric.
+    URL : str
+        The URl to the remote archive.
+    PATH_ARCHIVE : pathlib.Path
+        The directory to store the archive under.
+    unpack : bool
+        Whether to unpack the archive in place. Default is True.
+    remove : bool
+        Whether to remove the archive file after the download. Default is True.
     """
-    numeric_elements = []
 
-    for column in columns:
-        try:
-            float(column)
-        except ValueError:
-            continue
-        numeric_elements.append(column)
-    return numeric_elements
+    # Add filename
+    if "/pds/" in str(PATH_ARCHIVE):
+        PATH_ARCHIVE = PATH_ARCHIVE.parent / URL.split("/")[-1]
+    else:
+        PATH_ARCHIVE = PATH_ARCHIVE / URL.split("/")[-1]
+
+    download = Progress(
+        TextColumn("{task.fields[desc]}"),
+        BarColumn(bar_width=None),
+        DownloadColumn(),
+    )
+    with download as prog:
+        task = prog.add_task("download", desc=PATH_ARCHIVE.name, start=False)
+        success = copy_url(task, URL, PATH_ARCHIVE, prog)
+
+    if not success:
+        logger.critical(f"The URL {URL} is currently not reachable. Try again later.")
+        return False
+    if unpack:
+        if PATH_ARCHIVE.name.endswith(".tar.gz"):
+            with tarfile.open(PATH_ARCHIVE, mode="r:gz") as archive:
+                archive.extractall(PATH_ARCHIVE.parent)
+        elif PATH_ARCHIVE.name.endswith(".tar"):
+            with tarfile.open(PATH_ARCHIVE, mode="r") as archive:
+                archive.extractall(PATH_ARCHIVE.parent)
+        elif PATH_ARCHIVE.name.endswith(".zip"):
+            with ZipFile(PATH_ARCHIVE, "r") as archive:
+                archive.extractall(PATH_ARCHIVE.parent)
+    if remove:
+        PATH_ARCHIVE.unlink()
+    return True
+
+
+def copy_url(task, url, path, prog):
+    """Copy data from a url to a local file."""
+    try:
+        response = urlopen(url)
+    except urllib.error.URLError:
+        return False
+    # This will break if the response doesn't contain content length
+    prog.update(task, total=int(response.info()["Content-length"]))
+    with open(path, "wb") as dest_file:
+        prog.start_task(task)
+        for data in iter(partial(response.read, 32768), b""):
+            dest_file.write(data)
+            prog.update(task, advance=len(data))
+    return True
