5012c7311a103d501d45bf449a81e44deaac1f79
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-05-04 14:58:44 +0200

    Add classy status command

--- classy/cli.py
+++ classy/cli.py
@@ -3,10 +3,14 @@ import sys
 import webbrowser
 
 import click
+import rich
 import rocks
 
 import classy.classify
+from classy import cache
 from classy import core
+from classy import config
+from classy import index
 from classy.log import logger
 import classy.preprocessing
 from classy import taxonomies
@@ -37,6 +41,12 @@ def docs():
     type=click.Choice(taxonomies.SYSTEMS),
 )
 @click.option(
+    "--templates",
+    default=None,
+    help="Add class templates of the specified complex.",
+    type=click.Choice(taxonomies.COMPLEXES.keys()),
+)
+@click.option(
     "-s",
     "--source",
     type=click.Choice(sources.SOURCES),
@@ -49,11 +59,14 @@ def docs():
     help="Save plot to file in current working directory.",
 )
 @click.option("-v", is_flag=True, help="Set verbose output.")
-def spectra(id_, classify, taxonomy, source, save, v):
+def spectra(id_, classify, taxonomy, templates, source, save, v):
     """Retrieve, plot, classify spectra of an individual asteroid."""
 
     if v:
         classy.set_log_level("DEBUG")
+        rocks.set_log_level("DEBUG")
+    else:
+        rocks.set_log_level("ERROR")
 
     name, number = rocks.id(id_)
 
@@ -82,7 +95,47 @@ def spectra(id_, classify, taxonomy, source, save, v):
     if save:
         save = f"{number}_{name}_classy.png"
 
-    spectra.plot(taxonomy=taxonomy, save=save if save else None)
+    spectra.plot(taxonomy=taxonomy, save=save if save else None, templates=templates)
 
     if save:
         logger.info(f"Figure stored under {Path().cwd() / save}")
+
+
+@cli_classy.command()
+def status():
+    """Manage the index of asteroid spectra."""
+    from rich import prompt
+
+    # ------
+    # Echo inventory
+    cache.echo_inventory()
+
+    # ------
+    # Download all or clear
+    decision = prompt.Prompt.ask(
+        "Choose one of these actions:\n"
+        "[blue][0][/blue] Do nothing "
+        "[blue][1][/blue] Clear the cache "
+        "[blue][2][/blue] Retrieve all spectra",
+        choices=["0", "1", "2"],
+        show_choices=False,
+        default="0",
+    )
+
+    if decision == "1":
+        decision = prompt.Prompt.ask(
+            f"\nThis will delete the cache directory and all its contents. Are you sure?",
+            choices=["y", "n"],
+            show_choices=True,
+        )
+
+        if decision in ["y"]:
+            cache.remove()
+
+    elif decision == "2":
+        rich.print()
+        rocks.set_log_level("CRITICAL")
+        classy.set_log_level("CRITICAL")
+        sources._retrieve_spectra()
+
+        # cache.echo_inventory()
