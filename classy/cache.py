8ee6b377c4615de6073c41f12f028c842148022d
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-05-04 14:25:47 +0200

    Add inventory functions and change index look-up to global index

--- classy/cache.py
+++ classy/cache.py
@@ -1,27 +1,29 @@
 """Cache management for classy."""
 
 from pathlib import Path
+import shutil
 
 import numpy as np
 import pandas as pd
+import rich
 import rocks
 
 from classy import config
-from classy.log import logger
 from classy import core
+from classy.log import logger
+from classy import index
 from classy import sources
-
 
 # ------
 # Indeces of spectra
-def load_index(source):
-    """Load an index file."""
-    if source not in sources.SOURCES:
-        raise ValueError(
-            f"Unknown spectra source '{source}'. Choose one of {sources.SOURCES}."
-        )
-
-    return getattr(sources, source.lower()).load_index()
+# def load_index(source):
+#     """Load an index file."""
+#     if source not in sources.SOURCES:
+#         raise ValueError(
+#             f"Unknown spectra source '{source}'. Choose one of {sources.SOURCES}."
+#         )
+#
+#     return getattr(sources, source.lower()).load_index()
 
 
 def load_mahlke_index():
@@ -41,8 +43,37 @@ def load_spectra(idx_spectra):
     """
 
     spectra = [
-        getattr(sources, spec.source.lower()).load_spectrum(spec)
+        getattr(sources, spec.host.lower()).load_spectrum(spec)
         for _, spec in idx_spectra.iterrows()
     ]
 
     return spectra
+
+
+def remove():
+    """Remove the cache directory."""
+    shutil.rmtree(config.PATH_CACHE)
+
+
+def echo_inventory():
+
+    idx = index.load()
+    sources_ = list(idx.source.unique())
+
+    rich.print(
+        f"""\nContents of {config.PATH_CACHE}:
+
+    {len(idx)} asteroid reflectance spectra from {len(sources_)} sources
+      """
+    )
+
+    if sources_:
+        for i, (source, obs) in enumerate(
+            idx.sort_values("source").groupby("source"), 1
+        ):
+            rich.print(f"    {source:<8} {len(obs):>5}", end="")
+
+            if not i % 4:
+                rich.print()
+        else:
+            rich.print("\n")
