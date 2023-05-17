c539ea57d909fc98de314441867844825fefd833
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-05-17 10:57:07 +0200

    More code cleanup

--- classy/core.py
+++ classy/core.py
@@ -705,21 +705,6 @@ class Spectra(list):
         spectra = cache.load_spectra(spectra)
         return super().__init__(spectra)
 
-    # def __setitem__(self, index, item):
-    #     super().__setitem__(index, str(item))
-    #
-    # def insert(self, index, item):
-    #     super().insert(index, str(item))
-    #
-    # def append(self, item):
-    #     super().append(str(item))
-    #
-    # def extend(self, other):
-    #     if isinstance(other, type(self)):
-    #         super().extend(other)
-    #     else:
-    #         super().extend(str(item) for item in other)
-    #
     def __add__(self, rhs):
         if isinstance(rhs, Spectrum):
             rhs = [rhs]
