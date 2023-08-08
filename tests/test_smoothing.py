e37ef0bcf6452eaba57dc82b58d1a4973fd7257c
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-08 12:16:34 +0200

    Prepare unit test

---
+++ tests/test_smoothing.py
@@ -0,0 +1,7 @@
+"""Module to test classy smoothing routines."""
+
+# Verify that has_smoothing_parameters returns True if spectrum
+# present in smoothing.csv
+
+# Verify that savgol smoothing with fixed degree + window correctly sets
+# the reflectance of a given spectrum
