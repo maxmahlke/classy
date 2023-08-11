36aeae01dcd7f2a6abd753af51fc10ec1bb8233d
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-11 15:57:16 +0200

    Add progress module

---
+++ classy/progress.py
@@ -0,0 +1,9 @@
+"""Implement classy progress bars."""
+from rich import progress
+
+# Progress bar for MofN task counting
+mofn = progress.Progress(
+    progress.TextColumn("{task.description}", justify="right"),
+    progress.BarColumn(bar_width=None),
+    progress.MofNCompleteColumn(),
+)
