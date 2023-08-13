e267f88ccd332f376cc3377fcddf93da7c92717a
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-13 17:44:37 +0200

    Add missing parameter to new index

--- classy/index.py
+++ classy/index.py
@@ -29,7 +29,9 @@ def load():
             logger.error(
                 f"No reflectance spectra are available. Run '$ classy status' to retrieve them."
             )
-        return pd.DataFrame(data={"name": [], "source": [], "filename": []}, index=[])
+        return pd.DataFrame(
+            data={"name": [], "source": [], "filename": [], "host": []}, index=[]
+        )
 
     return pd.read_csv(PATH, dtype={"number": "Int64"}, low_memory=False)
 
