4cbb501cddc6329b0cbfbe6466d4fef873c258de
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-19 15:04:19 +0200

    Adapt SMASS to single load-data function

--- classy/sources/smass.py
+++ classy/sources/smass.py
@@ -4,10 +4,14 @@ import pandas as pd
 import rocks
 
 from classy import config
+from classy import cache
 from classy import index
 from classy.log import logger
+from classy import config
 from classy import tools
 
+# ------
+# Module definitions
 ARCH_DIR_REF_BIB = [
     ("smass1data_new", "smass1", "Xu+ 1995", "1995Icar..115....1X"),
     ("smass2data", "smass2", "Bus and Binzel 2002", "2002Icar..158..106B"),
@@ -19,81 +23,6 @@ ARCH_DIR_REF_BIB = [
     ("smassref8", "smassneoref8", "Binzel+ 2004", "2004Icar..170..259B"),
     ("smassref9", "hermesref9", "Rivkin+ 2004", "2004Icar..172..408R"),
 ]
-
-
-def _load_data(idx):
-    """Load data and metadata of a cached Gaia spectrum.
-
-    Parameters
-    ----------
-    idx : pd.Series
-        A row from the classy spectra index.
-
-    Returns
-    -------
-    pd.DataFrame, dict
-        The data and metadata. List-like attributes are in the dataframe,
-        single-value attributes in the dictionary.
-    """
-
-    # Load spectrum data file
-    PATH_DATA = config.PATH_CACHE / idx.filename
-    data = pd.read_csv(
-        PATH_DATA, names=["wave", "refl", "refl_err", "flag"], delimiter=r"\s+"
-    )
-
-    data["flag"] = [0 if f != 0 else 2 for f in data["flag"]]
-
-    # Adapt wavelength of smass1
-    if "/smass1/" in str(PATH_DATA):
-        if idx["name"] != "Schiaparelli":
-            data["wave"] /= 10000
-
-    # No metadata to return
-    return data, {}
-
-
-def load_obslog():
-    """Load the SMASS observation log from cache or from remote."""
-    PATH_LOG = config.PATH_CACHE / "smass/obslog.csv"
-    if not PATH_LOG.is_file():
-        tools._retrieve_from_github(host="smass", which="obslog", path=PATH_LOG)
-    return pd.read_csv(PATH_LOG)
-
-
-def get_id_from_filename(file_):
-    id_ = file_.name.split(".")[0]
-    id_ = id_.split("_")[0]
-
-    if id_ in AMBIGUOUS:
-        return AMBIGUOUS[id_]
-
-    if id_.endswith("visir8"):
-        id_ = id_[: -len("visir8")]
-    if id_.endswith("ir8"):
-        id_ = id_[: -len("ir8")]
-
-    # Asteroid Unnumbered: extract designation
-    match = None
-    if id_.startswith("au"):
-        id_ = id_.lstrip("au")
-        designation = re.match(
-            r"([1A][8-9][0-9]{2}[ _]?[A-Za-z]{2}[0-9]{0,3})|"
-            r"(20[0-9]{2}[_ ]?[A-Za-z]{2}[0-9]{0,3})",
-            id_,
-        )
-        match = designation.group(0)
-
-    # Asteroid: extract number
-    elif id_.startswith("a"):
-        id_ = id_.lstrip("a")
-        number = re.match(r"(\d\d\d\d\d\d)", id_)
-        if number:
-            match = number.group(0)
-    else:
-        match = id_
-    return match
-
 
 AMBIGUOUS = {
     # from MITHNEOS
@@ -114,34 +43,37 @@ AMBIGUOUS = {
     "a175706-obsB": 175706,
 }
 
+PATH = config.PATH_CACHE / "smass/"
 
+DATA_KWARGS = {"names": ["wave", "refl", "refl_err", "flag"], "delimiter": r"\s+"}
+
+
+# ------
+# Module functions
 def _retrieve_spectra():
     """Retrieve all SMASS spectra to smass/ the cache directory."""
 
     URL = "http://smass.mit.edu/data/smass"
 
     # Create directory structure and check if the spectrum is already cached
-    PATH_SMASS = config.PATH_CACHE / "smass/"
-    PATH_SMASS.mkdir(parents=True, exist_ok=True)
+    PATH.mkdir(parents=True, exist_ok=True)
 
     logger.info("Retrieving all SMASS reflectance spectra to cache...")
 
     for file_, _, _, _ in ARCH_DIR_REF_BIB:
         url_archive = f"{URL}/{file_}.tar.gz"
-        tools.download_archive(
-            url_archive, PATH_SMASS / f"{file_}.tar.gz", encoding="tar.gz"
-        )
+        tools.download_archive(url_archive, PATH / f"{file_}.tar.gz", encoding="tar.gz")
 
 
 def _build_index():
     # Add to global spectra index.
     entries = []
     logger.info("Indexing SMASS spectra...")
-    PATH_SMASS = config.PATH_CACHE / "smass/"
 
-    log = load_obslog()
+    log = cache.load_cat(host="smass", which="obslog")
+
     for _, dir, ref, bib in ARCH_DIR_REF_BIB:
-        PATH_DIR = PATH_SMASS / dir
+        PATH_DIR = PATH / dir
 
         for file_ in PATH_DIR.iterdir():
             # Skip splined fit ones
@@ -195,17 +127,59 @@ def _build_index():
                     "source": "SMASS",
                     "host": "smass",
                     "module": "smass",
-                    "public": True,
                 },
                 index=[0],
             )
 
-            data, _ = _load_data(entry.squeeze())
-            entry["wave_min"] = min(data["wave"])
-            entry["wave_max"] = max(data["wave"])
-            entry["N"] = len(data)
-
             entries.append(entry)
+
     entries = pd.concat(entries)
     index.add(entries)
     logger.info(f"Added {len(entries)} SMASS spectra to the classy index.")
+
+
+def _transform_data(idx, data):
+    data["flag"] = [0 if f != 0 else 2 for f in data["flag"]]
+
+    # Adapt wavelength of smass1
+    if "/smass1/" in idx.name:
+        if idx["name"] != "Schiaparelli":
+            data["wave"] /= 10000
+
+    # No metadata to record
+    meta = {}
+    return data, meta
+
+
+def get_id_from_filename(file_):
+    id_ = file_.name.split(".")[0]
+    id_ = id_.split("_")[0]
+
+    if id_ in AMBIGUOUS:
+        return AMBIGUOUS[id_]
+
+    if id_.endswith("visir8"):
+        id_ = id_[: -len("visir8")]
+    if id_.endswith("ir8"):
+        id_ = id_[: -len("ir8")]
+
+    # Asteroid Unnumbered: extract designation
+    match = None
+    if id_.startswith("au"):
+        id_ = id_.lstrip("au")
+        designation = re.match(
+            r"([1A][8-9][0-9]{2}[ _]?[A-Za-z]{2}[0-9]{0,3})|"
+            r"(20[0-9]{2}[_ ]?[A-Za-z]{2}[0-9]{0,3})",
+            id_,
+        )
+        match = designation.group(0)
+
+    # Asteroid: extract number
+    elif id_.startswith("a"):
+        id_ = id_.lstrip("a")
+        number = re.match(r"(\d\d\d\d\d\d)", id_)
+        if number:
+            match = number.group(0)
+    else:
+        match = id_
+    return match
