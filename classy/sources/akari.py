dc606f5d977e3a3dc962d9e390c74bcda1f6eaa7
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-19 15:03:59 +0200

    Adapt AKARI to single load-data function

--- classy/sources/akari.py
+++ classy/sources/akari.py
@@ -4,29 +4,73 @@ from classy import config
 from classy import index
 from classy import tools
 
+# ------
+# Module definitions
 SHORTBIB, BIBCODE = "Usui+ 2019", "2019PASJ...71....1U"
 
+PATH = config.PATH_CACHE / "akari"
 
-def _load_data(idx):
-    # data = sources._load_data(idx, DATA_KWARGS)
+DATA_KWARGS = {
+    "delimiter": "\s+",
+    "names": [
+        "wave",
+        "refl",
+        "refl_err",
+        "flag_err",
+        "flag_saturation",
+        "flag_thermal",
+        "flag_stellar",
+    ],
+}
 
-    # Load spectrum data file
-    PATH_DATA = config.PATH_CACHE / idx.filename
 
-    data = pd.read_csv(
-        PATH_DATA,
+# ------
+# Module functions
+def _retrieve_spectra():
+    """Download the AcuA-spec archive to cache."""
+    PATH.mkdir(parents=True, exist_ok=True)
+
+    URL = "https://darts.isas.jaxa.jp/pub/akari/AKARI-IRC_Spectrum_Pointed_AcuA_1.0/AcuA_1.0.tar.gz"
+    tools.download_archive(URL, PATH / "AcuA_1.0.tar.gz", encoding="tar.gz")
+
+
+def _build_index():
+    """Add the AKARI spectra to the classy spectra index."""
+
+    # Catch if download failed
+    if not (PATH / "AcuA_1.0/target.txt").is_file():
+        return
+
+    # Create index based on target file
+    entries = pd.read_csv(
+        PATH / "AcuA_1.0/target.txt",
         delimiter="\s+",
-        names=[
-            "wave",
-            "refl",
-            "refl_err",
-            "flag_err",
-            "flag_saturation",
-            "flag_thermal",
-            "flag_stellar",
-        ],
+        names=["number", "name", "obs_id", "date", "ra", "dec"],
+        dtype={"number": int},
+    )
+    entries = entries.drop_duplicates("number")
+
+    # Drop (4) Vesta and (4015) Wilson-Harrington as there are no spectra of them
+    entries = entries[~entries.number.isin([4, 4015])]
+
+    # Add filenames
+    entries["filename"] = entries.apply(
+        lambda row: f"akari/AcuA_1.0/reflectance/{row.number:>04}_{row['name']}.txt",
+        axis=1,
     )
 
+    entries["shortbib"] = SHORTBIB
+    entries["bibcode"] = BIBCODE
+    entries["date_obs"] = entries.date
+    entries["source"] = "AKARI"
+    entries["host"] = "AKARI"
+    entries["module"] = "akari"
+
+    # Et voila
+    index.add(entries)
+
+
+def _transform_data(_, data):
     # Add a joint flag, it's 1 if any other flag is 1
     data["flag"] = data.apply(
         lambda point: 1
@@ -39,74 +83,5 @@ def _load_data(idx):
     )
 
     # No metadata to record
-    return data, {}
-
-
-def _retrieve_spectra():
-    """Download the AcuA-spec archive to cache."""
-    URL = "https://darts.isas.jaxa.jp/pub/akari/AKARI-IRC_Spectrum_Pointed_AcuA_1.0/AcuA_1.0.tar.gz"
-
-    PATH_AKARI = config.PATH_CACHE / "akari"
-    PATH_AKARI.mkdir(parents=True, exist_ok=True)
-
-    # Retrieve spectra
-    tools.download_archive(URL, PATH_AKARI / "AcuA_1.0.tar.gz", encoding="tar.gz")
-
-
-def _build_index():
-    PATH_AKARI = config.PATH_CACHE / "akari"
-
-    # Catch if download failed
-    if not (PATH_AKARI / "AcuA_1.0/target.txt").is_file():
-        return
-
-    # Create index
-    akari = pd.read_csv(
-        PATH_AKARI / "AcuA_1.0/target.txt",
-        delimiter="\s+",
-        names=["number", "name", "obs_id", "date", "ra", "dec"],
-        dtype={"number": int},
-    )
-    akari = akari.drop_duplicates("number")
-
-    # Drop (4) Vesta and (4015) Wilson-Harrington as there are no spectra of them
-    akari = akari[~akari.number.isin([4, 4015])]
-
-    # Add filenames
-    akari["filename"] = akari.apply(
-        lambda row: f"{row.number:>04}_{row['name']}.txt", axis=1
-    )
-
-    akari = akari.drop(columns=["ra", "dec"])
-
-    # Create index to apped to classy index
-    entries = []
-
-    for _, row in akari.iterrows():
-        name, number = row["name"], row.number
-        filename = f"akari/AcuA_1.0/reflectance/{row.filename}"
-
-        # Append to index
-        entry = pd.DataFrame(
-            data={
-                "name": name,
-                "number": number,
-                "filename": filename,
-                "shortbib": SHORTBIB,
-                "bibcode": BIBCODE,
-                "date_obs": row.date,
-                "source": "AKARI",
-                "host": "AKARI",
-                "module": "akari",
-            },
-            index=[0],
-        )
-
-        data, _ = _load_data(entry.squeeze())
-        entry["wave_min"] = min(data["wave"])
-        entry["wave_max"] = max(data["wave"])
-        entry["N"] = len(data)
-        entries.append(entry)
-
-    entries = pd.concat(entries)
-    index.add(entries)
+    meta = {}
+    return data, meta
