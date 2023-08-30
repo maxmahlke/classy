c4d6e13ecc042d1695e36c765d94c63671dac982
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-30 10:51:13 +0200

    Add function to asyncronously query phase angles

--- classy/index.py
+++ classy/index.py
@@ -1,7 +1,10 @@
 """Module to manage the global spectra index in classy."""
-import sys
+import asyncio
+import aiohttp
 from datetime import datetime
+import io
 import functools
+import sys
 
 import numpy as np
 import pandas as pd
@@ -75,6 +78,12 @@ def add(entries):
         The entries to add to the index.
     """
 
+    # Add missing column
+    entries["phase"] = np.nan
+
+    # Convert all observation epochs to ISO-T format
+    entries["date_obs"] = entries["date_obs"].apply(lambda d: convert_to_isot(d))
+
     # Format for adding to index
     entries = entries.loc[
         :, [c for c in COLUMNS if c not in ["N", "wave_min", "wave_max"]]
@@ -98,38 +107,207 @@ def add(entries):
     save(index)
 
 
+# ------
+# Asynch phase angle query
+def get_or_create_eventloop():
+    """Enable asyncio to get the event loop in a thread other than the main thread
 
-def add_phase_angles():
-    """Add phase angles to all spectra in index."""
+    Returns
+    --------
+    out: asyncio.unix_events._UnixSelectorEventLoop
+    """
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
 
-    # Get the relevant rows from index: has observation date but no phase anlge yet
+
+def batch_phase():
     idx = load()
-    print(len(idx), "complete")
+
     idx["phase"] = np.nan
-    idx = idx.loc[(~pd.isna(idx.date_obs)) & (pd.isna(idx.phase))]
-    print(len(idx), "with obds date but no phase")
+    idx["epoch_im"] = np.nan
+    idx["sso"] = np.nan
+
+    # Get subindex with valid observation dates
+    idx_phase = idx.loc[~pd.isna(idx.date_obs)][["name", "date_obs"]]
+
+    # Exclude those with multiple date_obs for now
+    # These are done in a second loop and get an err_phase != 0
+    # idx_phase = idx_phase[~idx_phase.date_obs.str.contains(",")]
+
+    # idx_phase = idx_phase.loc[idx_phase["name"] == "Iva"]
+    # idx_phase = idx_phase.loc[idx_phase["name"].isin(["Iva"])]
+    # idx_phase = idx_phase.loc[idx_phase["number"] == 25]
+    # idx_phase = idx_phase[:500]
+
+    from rich.progress import Progress
+
+    with Progress(disable=False) as progress_bar:
+        progress = progress_bar.add_task(
+            "Add phase", total=len(idx_phase.groupby("name"))
+        )
+
+        # Run async loop to get ssoCard
+        loop = get_or_create_eventloop()
+        phases = loop.run_until_complete(
+            _get_datacloud_catalogue(idx_phase, progress_bar, progress)
+        )
+
+    # for entry in phases[0]:
+    # entry = list(entry)[0]
+    # index, phase, err_phase, epoch_im = entry
+    # for result in phases:
+    for index, phase, err_phase in phases:
+        # print(index, phase)
+        idx.loc[index, "phase"] = phase
+        idx.loc[index, "err_phase"] = err_phase
+        # idx.loc[index, "epoch_im"] = epoch_im
+        # print(index, phase, idx.loc[index, "date_obs"], epoch_im)
+        # idx.loc[index, "sso_im"] = sso_im
+
+    save(idx)
+
+
+async def _get_datacloud_catalogue(idx_phase, progress_bar, progress):
+    """Get catalogue asynchronously. First attempt local lookup, then query SsODNet.
+
+    Parameters
+    ----------
+    id_catalogue : list
+        Asteroid - catalogue combinations.
+    progress : bool or tdqm.std.tqdm
+        If progress is True, this is a progress bar instance. Else, it's False.
+    local : bool
+        If False, forces the remote query of the ssoCard. Default is True.
+
+    Returns
+    -------
+    list of dict
+        list containing len(id_) list with dictionaries corresponding to the
+    catalogues of the passed identifiers. If the catalogue is not available, the dict
+    is empty.
+    """
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout()) as session:
+        tasks = [
+            asyncio.ensure_future(
+                _local_or_remote_catalogue(ind, obs, session, progress_bar, progress)
+            )
+            # for name, obs in idx_phase.groupby("name")
+            for ind, obs in idx_phase.iterrows()
+        ]
+
+        results = await asyncio.gather(*tasks)
+    return results
+
+
+sema = asyncio.Semaphore(50)
+# lock = asyncio.Lock()
+
+
+async def _local_or_remote_catalogue(index, obs, session, progress_bar, progress):
+    """Check for presence of ssoCard in cache directory. Else, query from SsODNet."""
+    # print(obs)
+
+    # epochs = obs.date_obs.values
+    epoch = obs.date_obs
+    epochs = epoch.split(",")
+    name = obs["name"]
+    # index = obs.index
+
+    # Handle multi-epoch spectra by recording number of epochs per spectrum
+    # n_epochs = [len(epoch.split(",")) for epoch in epochs]
+    # epochs = [epoch.split(",") for epoch in epochs]
+    # epochs = [e for epoch in epochs for e in epoch]
+
+    async with sema:
+        phases = []
+        for epoch in epochs:
+            phase, _ = await _get_phase_angle(name, epoch, session)
+            phases.append(phase)
+        # phases, epoch_im = await _get_phase_angle(name, epochs, session)
+
+    # print(list(zip(epochs, epoch_im)))
+
+    # Average multi-epoch spectra
+    # from itertools import islice
+    #
+    # res = [
+    #     list(islice(phases, sum(n_epochs[:i]), sum(n_epochs[:i]) + ele))
+    #     for i, ele in enumerate(n_epochs)
+    # ]
+    #
+    # print(phases)
+    # breakpoint()
+    # print(list(zip(index, phases, epoch_im)))
+    # phases = [np.mean(r) for r in res]
+    # err_phases = [np.std(r) for r in res]
+
+    # err_phases = phases
+    phase = np.mean(phases)
+    err_phase = np.std(phases)
+    #
+    progress_bar.update(progress, advance=1)
+    return index, phase, err_phase
+
+
+async def _get_phase_angle(name, epochs, session):
+    """Query quaero and parse result for a single object.
+
+    Parameters
+    ----------
+    id_ssodnet : str
+        Asteroid ID from SsODNet.
+    catalogue : str
+        Datacloud catalogue name.
+    session : aiohttp.ClientSession
+        asyncio session
+
+    Returns
+    -------
+    dict
+        SsODNet response as dict if successful. Empty if query failed.
+    """
+    # Pass sorted list of epochs to speed up query
 
     # ------
-    # First round: those who have a single observation date
-    idx_single = idx[~idx.date_obs.str.contains(",")]
-    print(len(idx), "with single obs date")
+    # Query Miriade for phase angles
+    URL = "http://vo.imcce.fr/webservices/miriade/ephemcc_query.php"
 
-    # Send one query per asteroid
-    for name, spectra in idx_single.groupby("name"):
-        epochs = spectra["date_obs"].values
+    params = {
+        "-name": f"a:{name}",
+        "-mime": "json",
+        "-tcoor": "5",
+        "-tscale": "UTC",
+        "-ep": epochs,
+    }
 
-        if len(epochs) == 1:
-            continue
+    # print(params)
+    # epochs = {"epochs": io.StringIO("\n".join(epochs))}
+    # epochs_file = {"epochs": str.encode("\n".join(epochs))}
+    # async with session.post(url=URL, params=params, data=epochs_file) as response:
+    async with session.post(url=URL, params=params) as response:
+        response_json = await response.json()
+    # response = await
 
-        ephem = cache.miriade_ephems(name, epochs)
-        phase = ephem.phase.values
-        breakpoint()
+    # if not response.ok:
+    #     return {"data": {"phase": np.nan}}, ""
 
-    # ------
-    # Secound round: those who have more than one observation date
+    # response_json = await response.json()
+    phase = [data["phase"] for data in response_json["data"]]
+    # print(phase)
+    epoch_im = [data["epoch"][:-3] for data in response_json["data"]]
+    # if any(ep_im not in epochs for ep_im in epoch_im):
+    #     print(list(zip(epochs, epoch_im)))
+    #     breakpoint()
+    # # sso = response_json["sso"]["name"]
+    return phase, epoch_im  # , sso
 
 
-def convert_to_isot(dates, format):
+def convert_to_isot(dates):
     """Convert list of dates to ISOT format.
 
     Parameters
@@ -139,12 +317,32 @@ def convert_to_isot(dates, format):
     format : str
         The current format string of the dates.
     """
-    if isinstance(dates, str):
-        dates = [dates]
+    if pd.isna(dates) or not dates:
+        return ""
 
-    date_obs = ",".join(
-        [datetime.strptime(date, format).isoformat(sep="T") for date in dates]
-    )
+    if isinstance(dates, str):
+        dates = dates.split(",")
+
+    FORMATS = [
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y/%m/%d_%H:%M:%S",
+        "%Y-%m-%dT%H:%M",
+        "%Y-%m-%d %H:%M",
+        "%Y-%m-%d",
+    ]
+
+    for format in FORMATS:
+        try:
+            date_obs = ",".join(
+                [datetime.strptime(date, format).isoformat(sep="T") for date in dates]
+            )
+        except ValueError:
+            continue
+        else:
+            break
+    else:
+        raise ValueError(f"Unknown time format: {dates}. Expected ISO-T.")
+
     return date_obs
 
 
@@ -167,7 +365,7 @@ def store_smoothing(smoothing):
     """Store the feature index after copying metadata from the spectra index."""
     with np.errstate(invalid="ignore"):
         smoothing["number"] = smoothing["number"].astype("Int64")
-    smoothing.to_csv(PATH_SMOOTHING, index=True)
+    smoothing.to_csv(PATH_SMOOTHING, index=True, index_label="filename")
 
 
 def store_features(features):
@@ -180,5 +378,9 @@ def store_features(features):
 def load_features():
     """Load the feature index."""
     if not PATH_FEATURES.is_file():
-        return pd.DataFrame()
+        # Creating indices
+        ind = pd.MultiIndex(
+            levels=[[], []], codes=[[], []], names=["filename", "feature"]
+        )
+        return pd.DataFrame(index=ind)
     return pd.read_csv(PATH_FEATURES, index_col=["filename", "feature"])
