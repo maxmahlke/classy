211fa7faf1247a76f9082c12b311d85b5bf3794a
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-09-11 14:18:14 +0200

    Rename cache to data directory

--- classy/config.py
+++ classy/config.py
@@ -7,15 +7,15 @@ from classy.log import logger
 from platformdirs import user_cache_dir
 
 
-if "CLASSY_CACHE_DIR" in os.environ:
-    PATH_CACHE = Path(os.environ["CLASSY_CACHE_DIR"]).expanduser().absolute()
+if "CLASSY_DATA_DIR" in os.environ:
+    PATH_CACHE = Path(os.environ["CLASSY_DATA_DIR"]).expanduser().absolute()
 
     if not PATH_CACHE.is_dir():
         raise ValueError(f"Path {PATH_CACHE} does not exist.")
 else:
     PATH_CACHE = Path(user_cache_dir()) / "classy"
 
-logger.debug(f"classy cache directory: {PATH_CACHE}")
+logger.debug(f"classy data directory: {PATH_CACHE}")
 
 # Maximum missing wavelength range to extrapolate for classification
 EXTRAPOLATION_LIMIT = 10  # in percent
