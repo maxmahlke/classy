f620d90e9236c2502dc8bf85ad5922b1c5b7989a
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-10-12 18:17:00 +0200

    Fix first function test case by adding input

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -11,37 +11,37 @@ def test_interaction_with_index():
 
     # User Max wants to study K-type asteroids
     # He first checks that the classy data directory is populated
-    # using the `classy status` command
-    results = runner.invoke(cli.status)
+    # using the `classy status` command. It is, so he enters 0 to exit.
+    result = runner.invoke(cli.status, input="0\n")
     assert result.exit_code == 0
     assert "Contents of" in result.output
 
     # He starts by looking for spectra of Eos on the command line
-    results = runner.invoke(cli.spectra, ["eos"])
+    result = runner.invoke(cli.spectra, ["eos"])
     assert result.exit_code == 0
     assert "(221) Eos" in result.output
 
     # To confirm that (221) Eos is a K-type, he classifies the spectra
     # in the Mahlke scheme
-    results = runner.invoke(cli.classify, ["eos -t mahlke"])
+    result = runner.invoke(cli.classify, ["eos -t mahlke"])
     assert result.exit_code == 0
     assert "(221) Eos" in result.output
 
     # He wonders what Tholen class the Gaia spectrum has
-    results = runner.invoke(cli.classify, ["eos -s gaia -t tholen"])
+    result = runner.invoke(cli.classify, ["eos -s gaia -t tholen"])
     assert result.exit_code == 0
     assert "(221) Eos" in result.output
 
     # Max wonders how many spectra of K-type classifications there are
     # in his database
-    results = runner.invoke(cli.spectra, ["--family eos"])
+    result = runner.invoke(cli.spectra, ["--family eos"])
     assert result.exit_code == 0
     assert "(221) Eos" in result.output
 
     # He is most interested in spectra which cover the complete VisNIR range
 
     # As a first step, he wants to focus only on Eos family members
-    results = runner.invoke(
+    result = runner.invoke(
         cli.spectra, ["--family eos --wave_min 0.45 --wave_max 2.45"]
     )
     assert result.exit_code == 0
