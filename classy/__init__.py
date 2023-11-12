2e372d0c0332af5e448b48f2897b8f5143169437
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-11-12 16:06:54 +0100

    Minor edits

--- classy/__init__.py
+++ classy/__init__.py
@@ -3,9 +3,9 @@ import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from .log import set_log_level
-from .core import Spectrum
-from .core import Spectra
+from .log import set_log_level  # noqa
+from .core import Spectrum  # noqa
+from .core import Spectra  # noqa
 
 # Welcome to classy
 __version__ = "0.7"
