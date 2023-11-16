d5b786299b0f43416f0b89242e13c674c7d00224
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-11-16 16:11:51 +0100

    Catch Exception in case pyqtgraph is installed but pyside6 is not

--- classy/gui.py
+++ classy/gui.py
@@ -8,7 +8,7 @@ from classy.log import logger
 try:
     import pyqtgraph as pg
     from pyqtgraph.Qt import QtWidgets
-except ModuleNotFoundError as e:
+except (Exception, ModuleNotFoundError) as e:
     raise ModuleNotFoundError(
         "The Graphical User Interface of classy requires the pyqtgraph and pyside6 packages. "
         "Run 'pip install \"space-classy[gui]\" to install it.'"
